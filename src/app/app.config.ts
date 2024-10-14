157e99317bdd64d2a3bc2004f63bf4c22f6e6da7
rosana <rosmarielnunez@gmail.com>
2024-10-14 10:08:38 +0200

    Update components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+              provideRouter(routes),
+              provideAnimationsAsync(),
+              MatIconModule,
+              MatToolbarModule,
+              MatButtonModule
+  ]
 };
